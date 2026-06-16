5cc215fca1341054b79294114ec8ffcbf6d66bca
Alister1-code <aaronlister55@gmail.com>
2026-06-16 05:51:01 +0100

    ignore file

--- main.rs
+++ main.rs
@@ -13,7 +13,6 @@ fn main() {
     let bundle_path_str = &args[1];
     let bundle_path = Path::new(bundle_path_str);
 
-    // 1. Basic Linux Sanity Checks
     if !bundle_path.is_dir() {
         eprintln!("Error: '{}' is not a valid bundle directory.", bundle_path_str);
         std::process::exit(1);
@@ -24,11 +23,8 @@ fn main() {
         std::process::exit(1);
     }
 
-    // 2. Define the internal executable path
     let relative_exec_path = bundle_path.join("AppRun");
 
-    // Canonicalize turns this into an absolute path and verifies it exists.
-    // If the file doesn't exist, canonicalize will return an Error.
     let executable_path = match std::fs::canonicalize(&relative_exec_path) {
         Ok(path) => path,
         Err(_) => {
@@ -42,17 +38,14 @@ fn main() {
 
     println!("Launching bundle: {}...", bundle_path_str);
 
-    // 3. Execute the internal binary
     let app_args: Vec<&String> = args.iter().skip(2).collect();
 
     let mut child = Command::new(&executable_path)
         .args(&app_args)
-        // This will now work perfectly because executable_path is absolute
         .current_dir(bundle_path) 
         .spawn()
         .expect("Failed to start the bundle application");
 
-    // 4. Wait for the application to finish
     match child.wait() {
         Ok(status) => {
             if let Some(code) = status.code() {
